d4727c3ba313f22e4f995f24a160d1e21b32596b
AhmadSyamsuddin <asyam5214@gmail.com>
2025-11-12 19:06:41 +0700

    Refactor user registration logic and validation in UserModel

--- my-app/app/api/register/route.ts
+++ my-app/app/api/register/route.ts
@@ -1,19 +1,19 @@
-// import UserModel from "@/db/models/UserModel";
-// import errorHandler from "@/helpers/errHandler";
+import UserModel from "@/db/models/UserModel";
+import errorHandler from "@/helpers/errHandler";
 
-// export async function POST(request: Request) {
-//     const { fullname, name, email, password } = await request.json();
-//   try {
-//     const data = await UserModel.create({ fullname, name, email, password });
-//     return Response.json(
-//       {
-//         message: "User registered successfully",
-//         data,
-//       },
-//       { status: 201 }
-//     );
-
-//   } catch (error) {
-//     return errorHandler(error);
-//   }
-// }
+export async function POST(request: Request) {
+  const { fullname, email, password } = await request.json();
+  console.log({ fullname, email, password }, "<<< REGISTER DATA");
+  try {
+    const data = await UserModel.create({ fullname, email, password });
+    return Response.json(
+      {
+        message: "User registered successfully",
+        data,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
